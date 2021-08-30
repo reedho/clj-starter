0f7a2a16133db66041da1fc8c8f7903bae3aeaa3
Muhammad Ridho <reedho@gmail.com>
2021-08-30 15:31:40 +0700

    Ring, the the Ruby's Rack of Clojure

--- src/user.clj
+++ src/user.clj
@@ -1,4 +1,19 @@
-(ns user)
+(ns user
+  (:require [ring.adapter.jetty :as jetty]))
 
 (defn hello []
   (println "Hello world"))
+
+(defn handler [req]
+  {:status  200
+   :headers {"Content-Type" "text/plain"}
+   :body    "Hello world"})
+
+(comment
+  ;; Start jetty web server
+  (def _server
+    (jetty/run-jetty handler {:port 3330 :join? false}))
+
+  ;; Stop the server
+  (.stop _server)
+  )
