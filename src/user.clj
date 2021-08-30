875cb4479bac38e3ff023aa449ba8aa55158e693
Muhammad Ridho <reedho@gmail.com>
2021-08-30 15:51:33 +0700

    ring.util.response

--- src/user.clj
+++ src/user.clj
@@ -1,5 +1,6 @@
 (ns user
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.util.response]))
 
 (defn hello []
   (println "Hello world"))
@@ -9,6 +10,12 @@
    :headers {"Content-Type" "text/plain"}
    :body    "Hello world"})
 
+(defn handler1 [req]
+  {:status  200
+   :headers {"Content-Type" "text/plain"}
+   :body    (-> (ring.util.response/response "Hello world")
+                (ring.util.response/content-type "text/plain"))})
+
 (comment
   ;; Start jetty web server
   (def _server
@@ -16,4 +23,14 @@
 
   ;; Stop the server
   (.stop _server)
+  )
+
+
+(comment
+  (require '[ring.util.response])
+  (ring.util.response/response "Hello world")
+  (ring.util.response/header {} "Content-Type" "text/plain")
+  (ring.util.response/content-type {} "text/plain")
+  (ring.util.response/redirect "https://google.com")
+  (ring.util.response/file-response "README.md")
   )
